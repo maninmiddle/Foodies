e0d24ea7e2cb4c62ea3e6c34d1116554579d90a0
maninmiddle <maninmiddler@gmail.com>
2024-11-13 08:33:58 +0300

    Initial commit

---
+++ app/src/main/java/com/maninmiddle/foodies/presentation/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.maninmiddle.foodies.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.maninmiddle.foodies.ui.theme.FoodiesTheme
+import org.koin.androidx.compose.KoinAndroidContext
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FoodiesTheme {
+                KoinAndroidContext {
+
+                }
+            }
+        }
+    }
+}
+
